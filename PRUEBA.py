629454d0a7ddf017a8483802f3a68e561d647028
Iorsharck <56987309+Iorsharck@users.noreply.github.com>
2023-11-21 03:38:25 -0600

    Update PRUEBA.py

--- PRUEBA.py
+++ PRUEBA.py
@@ -1,56 +1,81 @@
 import requests
 from openpyxl import Workbook
-
-def buscarguardar(nombre_pokemon):
-    
-    if nombre_pokemon != "":
-
-      url = f"https://pokeapi.co/api/v2/pokemon/{nombre_pokemon}"
-      response = requests.get(url)
-
-      if response.status_code == 200 :
-          datos = response.json()
-
-          nombre = datos["name"]
-          habilidades = []
-          habilidadoc= []
-          tipos = [tipo["type"]["name"] for tipo in datos["types"]]
+	
+def buscar(nombre_pokemon):
+	
+  if nombre_pokemon != "":
+		
+    url = f"https://pokeapi.co/api/v2/pokemon/{nombre_pokemon}"
+    response = requests.get(url)
+    informacion = []
+		
+    if response.status_code == 200 :
+			
+      datos = response.json()
+      nombre = datos["name"]
+      habilidades = []
+      habilidad_oc= ""
+      tipos = [tipo["type"]["name"] for tipo in datos["types"]]
+      for habilidad in datos["abilities"]: 
+				
+        if habilidad["is_hidden"]:
+          habilidad_oc = habilidad["ability"]["name"]
+        else:
+          habilidades.append(habilidad["ability"]["name"])
       
-          for habilidad in datos["abilities"]:
-              if habilidad["is_hidden"]:
-                  habilidadoc.append(habilidad["ability"]["name"])
-              else:
-                  habilidades.append(habilidad["ability"]["name"])
-                  
-          
-          print(f"Nombre: {nombre}")
-          print("Habilidades:", ", ".join(habilidades))
-          print("Habilidad Oculta:", ",".join(habilidadoc))
-          print("Tipos:", ", ".join(tipos))
-          
-          libro_trabajo = Workbook()
-          hoja = libro_trabajo.active
+      if habilidad_oc == "": habilidad_oc = "No tiene"
+      informacion = [nombre, [], [], habilidad_oc]
+      informacion[1].extend(tipos)
+      informacion[2].extend(habilidades)
+        
 
-          
-          hoja['A1'] = "Nombre"
-          hoja['B1'] = "Habilidades"
-          hoja['C1'] = "Habilidad Oculta"
-          hoja['D1'] = "Tipos"
-          
-          hoja.append([nombre, ", ".join(habilidades), ",".join(habilidadoc), ", ".join(tipos)])
+      return informacion
 
-          libro_trabajo.save(f"{nombre}.xlsx")
-          print(f"La información de {nombre} se ha guardado en {nombre}.xlsx")
-      else:
-          print(f"No se encontró ningún Pokémon con el nombre o número '{nombre_pokemon}'.")
-          nombre = input("Ingresa el nombre o número del Pokémon: ")
-          buscarguardar(nombre)
 
     else:
-        print(f"Favor de ingresar los datos pedidos")
-        nombre = input("Ingresa el nombre o número del Pokémon: ")
-        buscarguardar(nombre)
+      print(f"No se encontró ningún Pokémon con el nombre o número '{nombre_pokemon}'.")
+      nombre = input("Ingresa el nombre o número del Pokémon: ")
+      return buscar(nombre)
+
+  else:
+    print(f"Favor de ingresar los datos pedidos")
+    nombre = input("Ingresa el nombre o número del Pokémon: ")
+    return buscar(nombre)
+
+
+def imprimir(info):
+	nombre = info[0]
+	tipos = info[1]
+	habilidades = info[2]
+	habilidad_oc = info[3]
+	print(f"Nombre: {nombre}")
+	print("Tipos:", ", ".join(tipos))
+	print("Habilidades:", ", ".join(habilidades))
+	print("Habilidad Oculta:", habilidad_oc)
+
+
+def excel(info):
+	nombre = info[0]
+	tipos = info[1]
+	habilidades = info[2]
+	habilidad_oc = info[3]
+	
+	libro_trabajo = Workbook()
+	hoja = libro_trabajo.active
+	
+	hoja['A1'] = "Nombre"
+	hoja['B1'] = "Tipos"
+	hoja['C1'] = "Habilidades"
+	hoja['D1'] = "Habilidad Oculta"
+	
+	hoja.append([nombre, ", ".join(tipos), ", ".join(habilidades),habilidad_oc])
+
+	libro_trabajo.save(f"{nombre}.xlsx")
+	libro_trabajo.close()
+	print(f"La información de {nombre} se ha guardado en {nombre}.xlsx")
 
 pokemon = input("Ingresa el nombre o número del Pokémon: ")
-buscarguardar(pokemon)
+inf = buscar(pokemon)
+imprimir(inf)
+excel(inf)
 
